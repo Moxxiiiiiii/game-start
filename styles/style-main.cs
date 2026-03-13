83b685d34989d83fc7949e708b29dfee3fbdd9b9
Moxxiiiiiii <moxxiiiiiii0212@gmail.com>
2026-03-13 20:37:49 +1100

    Created desired layout png + html doc

---
+++ styles/style-main.cs
@@ -0,0 +1,21 @@
+body {
+    background-color: #BFFFDD;
+}
+
+.contain-all {
+    display: flex;
+    flex-direction: row;
+}
+
+.left-side {
+    background-color: #FFEABF;
+    height: 300px;
+    width: 300px;
+    margin-right: 30px;
+}
+
+.right-side {
+    background-color: #FFEABF;
+    height: 300px;
+    width: 300px;
+}
